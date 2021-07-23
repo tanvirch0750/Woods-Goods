7a25e026c3a46fc6fbd53da8af0fe93015790cbf
tanvirch0750 <tanvir0750@yahoo.com>
2021-07-23 10:45:30 +0600

    completed footer section

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import Wrapper from './Footer.styles';
 
 function Footer() {
-    return <div>Footer</div>;
+    return (
+        <Wrapper>
+            <h5>
+                &copy;{new Date().getFullYear()} <span>Woods & Goods</span>
+            </h5>
+            <h5>All rights reserve</h5>
+        </Wrapper>
+    );
 }
 
 export default Footer;

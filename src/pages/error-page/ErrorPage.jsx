dc038b27ff6155c8c2e19d1193c0bf8579964ab5
tanvirch0750 <tanvir0750@yahoo.com>
2021-07-24 20:58:16 +0600

    implemented error page

--- src/pages/error-page/ErrorPage.jsx
+++ src/pages/error-page/ErrorPage.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Wrapper from './ErrorPage.styles';
 
 function ErrorPage() {
-    return <div>Error Page</div>;
+    return (
+        <Wrapper className="page-100">
+            <section>
+                <h1>404</h1>
+                <h3>Sorry, the page you tried cannot be found</h3>
+                <Link to="/" className="btn">
+                    Back Home
+                </Link>
+            </section>
+        </Wrapper>
+    );
 }
 
 export default ErrorPage;

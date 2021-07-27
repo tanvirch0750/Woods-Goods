600312e6972fd9f1cec819a3e7cfeddd14c2fe5f
tanvirch0750 <tanvir0750@yahoo.com>
2021-07-28 00:02:25 +0600

    completed contact component

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import Wrapper from './Contact.styles';
 
 function Contact() {
-    return <div>Contact</div>;
+    return (
+        <Wrapper>
+            <div className="section-center">
+                <h3>Join our news letter and get 20% off!</h3>
+                <div className="content">
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae impedit
+                        corporis est reiciendis tenetur a culpa suscipit repellat minus at.
+                    </p>
+                    <form className="contact-form">
+                        <input type="email" className="form-input" placeholder="Enter your email" />
+                        <button type="submit" className="submit-btn">
+                            Subscribe
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </Wrapper>
+    );
 }
 
 export default Contact;
--- src/components/contact/Contact.styles.js
+++ src/components/contact/Contact.styles.js
@@ -4,9 +4,10 @@ const Wrapper = styled.section`
     padding: 5rem 0;
     h3 {
         text-transform: none;
+        color: var(--clr-primary-5);
     }
     p {
-        line-height: 2;
+        line-height: 1.5;
         max-width: 45em;
         color: var(--clr-grey-5);
     }
@@ -21,7 +22,7 @@ const Wrapper = styled.section`
     .submit-btn {
         font-size: 1rem;
         padding: 0.5rem 1rem;
-        border: 2px solid var(--clr-black);
+        border: 2px solid var(--clr-primary-5);
     }
     .form-input {
         border-right: none;
@@ -29,12 +30,15 @@ const Wrapper = styled.section`
         border-top-left-radius: var(--radius);
         border-bottom-left-radius: var(--radius);
     }
+    .form-input:focus{
+        outline: none;
+    }
     .submit-btn {
         border-top-right-radius: var(--radius);
         border-bottom-right-radius: var(--radius);
     }
     .form-input::placeholder {
-        color: var(--clr-black);
+        color: var(--clr-grey-5);
         text-transform: capitalize;
     }
     .submit-btn {
@@ -44,6 +48,9 @@ const Wrapper = styled.section`
         cursor: pointer;
         transition: var(--transition);
         color: var(--clr-black);
+    }
+    .submit-btn:focus{
+        outline: none;
     }
     .submit-btn:hover {
         color: var(--clr-white);

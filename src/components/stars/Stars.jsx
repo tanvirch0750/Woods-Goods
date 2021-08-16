befdbde872c22c141713601987d99394ce2d3cac
tanvirch0750 <tanvir0750@yahoo.com>
2021-08-16 23:32:50 +0600

    implemented single product stars component

--- src/components/stars/Stars.jsx
+++ src/components/stars/Stars.jsx
@@ -1,7 +1,31 @@
+/* eslint-disable no-nested-ternary */
 import React from 'react';
+import { BsStar, BsStarFill, BsStarHalf } from 'react-icons/bs';
+import Wrapper from './Stars.styles';
 
-function Stars() {
-    return <div>Stars</div>;
+function Stars({ stars, reviews }) {
+    const tempStars = Array.from({ length: 5 }, (_, index) => {
+        const number = index + 0.5;
+
+        return (
+            <span key={index}>
+                {stars >= index + 1 ? (
+                    <BsStarFill />
+                ) : stars >= number ? (
+                    <BsStarHalf />
+                ) : (
+                    <BsStar />
+                )}
+            </span>
+        );
+    });
+
+    return (
+        <Wrapper>
+            <div className="stars">{tempStars}</div>
+            <p className="reviews">({reviews} customer reviews)</p>
+        </Wrapper>
+    );
 }
 
 export default Stars;
--- src/pages/single-product-page/SingleProductPage.jsx
+++ src/pages/single-product-page/SingleProductPage.jsx
@@ -56,7 +56,7 @@ function SingleProductPage() {
                     <ProductImages images={images}/>
                     <section className="content">
                         <h2>{name}</h2>
-                        <Stars />
+                        <Stars stars={stars} reviews={reviews}/>
                         <h5 className="price">{formatPrice(price)}</h5>
                         <p className="desc">{description}</p>
                         <p className="info">

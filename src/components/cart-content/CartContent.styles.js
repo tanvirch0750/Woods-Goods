92b51f08448193b63c8673274394df315a41bf32
tanvirch0750 <tanvir0750@yahoo.com>
2021-10-09 00:51:35 +0600

    implemented cart totals component

--- src/components/cart-content/CartContent.styles.js
+++ src/components/cart-content/CartContent.styles.js
@@ -10,16 +10,24 @@ const Wrapper = styled.section`
         background: transparent;
         border-color: transparent;
         text-transform: capitalize;
-        padding: 0.25rem 0.5rem;
+        padding: 0.4rem 0.8rem;
         background: var(--clr-primary-5);
         color: var(--clr-white);
         border-radius: var(--radius);
-        letter-spacing: var(--spacing);
         font-weight: 400;
+        transition: var(--transition);
         cursor: pointer;
     }
+    .link-btn:hover {
+        color: var(--clr-primary-10);
+        background: var(--clr-primary-1);
+    }
     .clear-btn {
-        background: var(--clr-black);
+        background: #333
+    }
+    .clear-btn:hover {
+        color: var(--clr-primary-10);
+        background: #444
     }
 `;
 export default Wrapper;
--- src/components/cart-totals/CartTotals.jsx
+++ src/components/cart-totals/CartTotals.jsx
@@ -1,7 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { useCartContext } from '../../context/cartContext';
+import { formatPrice } from '../../utils/helpers';
+import Wrapper from './CartTotals.styles';
 
 function CartTotals() {
-    return <div>Cart totals</div>;
+    const { totalAmount, shippingFee } = useCartContext();
+
+    return (
+        <Wrapper>
+            <div>
+                <article>
+                    <h5>
+                        Subtotal : <span>{formatPrice(totalAmount)}</span>
+                    </h5>
+                    <p>
+                        Shipping Fee: <span>{formatPrice(shippingFee)}</span>
+                    </p>
+                    <hr />
+                    <h4>
+                        Order Total: <span>{formatPrice(totalAmount + shippingFee)}</span>
+                    </h4>
+                </article>
+                <Link to="/checkout" className="btn">
+                    Proceed to checkout
+                </Link>
+            </div>
+        </Wrapper>
+    );
 }
 
 export default CartTotals;

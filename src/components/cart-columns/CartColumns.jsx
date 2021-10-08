ceb5580e823dd6de62760caafd8b4b20804b8e49
tanvirch0750 <tanvir0750@yahoo.com>
2021-10-09 00:31:34 +0600

    implemented cart columns component

--- src/components/cart-columns/CartColumns.jsx
+++ src/components/cart-columns/CartColumns.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import Wrapper from './CartColumns.styles';
 
 function CartColumns() {
-    return <div>cart columns</div>;
+    return (
+        <Wrapper>
+            <div className="content">
+                <h5>Item</h5>
+                <h5>Price</h5>
+                <h5>Quantity</h5>
+                <h5>Subtotal</h5>
+                <span />
+            </div>
+            <hr />
+        </Wrapper>
+    );
 }
 
 export default CartColumns;
--- src/components/cart-columns/CartColumns.styles.js
+++ src/components/cart-columns/CartColumns.styles.js
@@ -10,7 +10,7 @@ const Wrapper = styled.div`
             justify-items: center;
             column-gap: 1rem;
             h5 {
-                color: var(--clr-grey-5);
+                color: var(--clr-grey-2);
                 font-weight: 400;
             }
         }

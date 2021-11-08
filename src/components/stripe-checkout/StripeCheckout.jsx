61c4b60ef54d59c748419862b5ed226b774289c4
tanvirch0750 <tanvir0750@yahoo.com>
2021-11-08 20:21:39 +0600

    Import stripe checkout variables

--- src/components/stripe-checkout/StripeCheckout.jsx
+++ src/components/stripe-checkout/StripeCheckout.jsx
@@ -1,12 +1,35 @@
+/* eslint-disable import/extensions */
+/* eslint-disable prettier/prettier */
+/* eslint-disable import/order */
+/* eslint-disable no-unused-vars */
+import {
+    Elements
+} from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 import React from 'react';
 import Wrapper from './StripeCheckout.styles';
+// import {
+//     CardElement,
+//     useStripe,
+//     Elements,
+//     useElements,
+//   } from '@stripe/react-stripe-js'
+// import axios from 'axios'
+// import { useCartContext } from '../../context/cartContext';
+// import { useUserContext } from '../../context/userContext'
+// import { formatPrice } from '../../utils/helpers'
+// import { useHistory } from 'react-router-dom'
+
+const promise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY)
 
 const CheckoutForm = () => <h2>Hello stripe checkout World</h2>;
 
 function StripeCheckout() {
     return (
         <Wrapper>
-            <CheckoutForm />
+            <Elements stripe={promise} >
+               <CheckoutForm />
+            </Elements>  
         </Wrapper>
     );
 }

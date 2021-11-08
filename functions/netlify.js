f4e95b5d67dca7e5736f9f2534c267dc9b1ad928
tanvirch0750 <tanvir0750@yahoo.com>
2021-11-08 17:06:44 +0600

    Setup first netlify function

---
+++ functions/netlify.js
@@ -0,0 +1,8 @@
+/* eslint-disable func-names */
+/* eslint-disable no-unused-vars */
+exports.handler = async function(event, context) {
+   return {
+      statusCode: 200,
+      body: "Hello Netlify function"
+   }
+}

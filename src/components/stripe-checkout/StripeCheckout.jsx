01a5fa01cead56e2fd8f391e80c097f3a21a899f
tanvirch0750 <tanvir0750@yahoo.com>
2021-11-08 20:47:45 +0600

    setup stripe checkout state variables

--- src/components/stripe-checkout/StripeCheckout.jsx
+++ src/components/stripe-checkout/StripeCheckout.jsx
@@ -1,13 +1,9 @@
-/* eslint-disable import/extensions */
-/* eslint-disable prettier/prettier */
-/* eslint-disable import/order */
 /* eslint-disable no-unused-vars */
-import {
-    Elements
-} from '@stripe/react-stripe-js';
+import { Elements, useElements, useStripe } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
-import React from 'react';
-import Wrapper from './StripeCheckout.styles';
+import React, { useState } from 'react';
+// import { formatPrice } from '../../utils/helpers'
+import { useHistory } from 'react-router-dom';
 // import {
 //     CardElement,
 //     useStripe,
@@ -15,21 +11,53 @@ import Wrapper from './StripeCheckout.styles';
 //     useElements,
 //   } from '@stripe/react-stripe-js'
 // import axios from 'axios'
-// import { useCartContext } from '../../context/cartContext';
-// import { useUserContext } from '../../context/userContext'
-// import { formatPrice } from '../../utils/helpers'
-// import { useHistory } from 'react-router-dom'
+import { useCartContext } from '../../context/cartContext';
+import { useUserContext } from '../../context/userContext';
+import Wrapper from './StripeCheckout.styles';
 
-const promise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY)
+const promise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY);
 
-const CheckoutForm = () => <h2>Hello stripe checkout World</h2>;
+const CheckoutForm = () => {
+    const { cart, totalAmount, shippingFee, clearCart } = useCartContext();
+    const { myUser } = useUserContext();
+    const history = useHistory();
+
+    // STRIPE STUFF
+    const [succeeded, setSucceeded] = useState(false);
+    const [error, setError] = useState(null);
+    const [processing, setProcessing] = useState('');
+    const [disabled, setDisabled] = useState(true);
+    const [clientSecret, setClientSecret] = useState('');
+    const stripe = useStripe();
+    const elements = useElements();
+
+    const cardStyle = {
+        style: {
+            base: {
+                color: '#32325d',
+                fontFamily: 'Arial, sans-serif',
+                fontSmoothing: 'antialiased',
+                fontSize: '16px',
+                '::placeholder': {
+                    color: '#32325d',
+                },
+            },
+            invalid: {
+                color: '#fa755a',
+                iconColor: '#e4c5be',
+            },
+        },
+    };
+
+    return <h2>Hello stripe checkout World</h2>;
+};
 
 function StripeCheckout() {
     return (
         <Wrapper>
-            <Elements stripe={promise} >
-               <CheckoutForm />
-            </Elements>  
+            <Elements stripe={promise}>
+                <CheckoutForm />
+            </Elements>
         </Wrapper>
     );
 }
